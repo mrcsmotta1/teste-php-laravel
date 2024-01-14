b4a0e2da2e87b824ef14275afd83f1dfac42da91
Marcos Motta <mrcsmotta1@gmail.com>
2024-01-14 12:14:09 -0300

    Criado rota, validação aceitar apenas arquivo json, view, controller e job para upload do json

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+<!-- Em resources/views/layouts/app.blade.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Aplicação</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="{{ route('index') }}">Minha Aplicação</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('import.upload.form') }}">Importar Arquivo</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+
+<div class="container mt-4">
+    @yield('content')
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Adicione outros scripts necessários aqui -->
+
+</body>
+</html>
