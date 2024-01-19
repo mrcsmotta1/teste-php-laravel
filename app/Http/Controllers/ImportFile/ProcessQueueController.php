baf24147c3d9aa26a598ce2361b05b56b74f994c
Marcos Motta <mrcsmotta1@gmail.com>
2024-01-18 23:23:32 -0300

    Inserindo log e informação com o exercicio de todos os arquivos processados

--- app/Http/Controllers/ImportFile/ProcessQueueController.php
+++ app/Http/Controllers/ImportFile/ProcessQueueController.php
@@ -28,16 +28,18 @@ class ProcessQueueController extends Controller
         $connection = 'database';
 
         $result = false;
+        $exercicioProcessado = [];
         while ($job = Queue::connection($connection)->pop('file_processing_queue')) {
             $rs = unserialize($job->payload()['data']['command']);
 
             if (empty($rs->data)) {
-                Log::error("Erro ao tentar o unserialize no arquivo, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+                Log::error("Erro ao tentar o unserialize no arquivo, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             }
 
             if (!empty($rs->data)) {
                 $exercicio = trim($rs->data['exercicio']);
-                Log::info("Unserialize do arquivo: {$exercicio}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+                array_push($exercicioProcessado, $exercicio);
+                Log::info("Unserialize do arquivo: {$exercicio}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
 
                 foreach ($rs->data['documentos'] as $key => $documento) {
                     $result = $this->processDocument($documento, $exercicio);
@@ -48,11 +50,12 @@ class ProcessQueueController extends Controller
         }
 
         if (!$result) {
-            Log::error("Erro ao processar a fila, verifique o conteudo do arquivo e importe novamente, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+            Log::error("Erro ao processar a fila, verifique o conteudo do arquivo e importe novamente, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             return redirect()->route('import.process.queue.form')->with('error', 'Erro ao processar a fila, verifique o conteudo do arquivo e importe novamente.');
         }
 
-        Log::info("Fila processada com sucesso arquivo: {$exercicio}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+        $exercicioString = implode(' - ', $exercicioProcessado);
+        Log::info("Fila processada com sucesso arquivo: {$exercicioString}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
         return redirect()->route('import.process.queue.form')->with('success', 'Fila processada com sucesso.');
     }
 
@@ -61,23 +64,23 @@ class ProcessQueueController extends Controller
         $dataLower = $this->dataLower($data);
 
         if ($dataLower['categoria'] !== 'remessa' && $dataLower['categoria'] !== 'remessa parcial') {
-            Log::error("Categoria inválida: {$dataLower['categoria']}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+            Log::error("Categoria inválida: {$dataLower['categoria']}, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             return false;
         }
 
         if ($dataLower['categoria'] === 'remessa' && strpos($dataLower['titulo'], 'semestre') === false) {
-            Log::error("Categoria Remessa inválida sem Semestre, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+            Log::error("Categoria Remessa inválida sem Semestre, Categoria: {$dataLower['categoria']} -- Titulo: {$dataLower['titulo']} " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             return false;
         }
         if ($dataLower['categoria'] === 'remessa parcial' && $this->isValidMonth($dataLower['titulo']) === false) {
-            Log::error("Categoria Remessa Parcial inválida sem nome de um mês válido, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+            Log::error("Categoria Remessa Parcial inválida sem nome de um mês válido, Categoria: {$dataLower['categoria']} -- Titulo: {$dataLower['titulo']}" . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             return false;
         }
 
         $categoryID = $this->getCategoryId($dataLower['categoria']);
 
         if (!$categoryID) {
-            Log::error("Error ao selecionar categoria do arquivo {$exercicio} no BD!, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+            Log::error("Error ao selecionar categoria do arquivo {$exercicio} no BD!, Categoria: {$dataLower['categoria']} -- Titulo: {$dataLower['titulo']}  " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
             return false;
         }
 
@@ -88,7 +91,7 @@ class ProcessQueueController extends Controller
             'contents' => $data['conteúdo'],
         ])->id;
 
-        Log::info("Documento id:{$newDocumentId} salvo com sucesso!, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__);
+        Log::info("Documento id:{$newDocumentId} salvo com sucesso!, " . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
 
         return true;
     }
