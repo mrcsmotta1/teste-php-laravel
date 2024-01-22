efaf119f0315985c1f7d0c5e69b44f21bf845614
Marcos Motta <mrcsmotta1@gmail.com>
2024-01-22 15:34:04 -0300

    Update no validate para verificar apenas a validação de json

--- app/Rules/JsonValidationRule.php
+++ app/Rules/JsonValidationRule.php
@@ -7,12 +7,6 @@ use Illuminate\Contracts\Validation\Rule;
 
 class JsonValidationRule implements Rule
 {
-
-    public $errorMax;
-    public $error;
-    public function __construct(protected $maxSize)
-    {
-    }
     public function passes($attribute, $value)
     {
         $mimeType = $value->getClientMimeType();
@@ -23,27 +17,13 @@ class JsonValidationRule implements Rule
         }
 
         $content = $value->getContent();
-        $result = json_decode($content);
-
-        if (json_last_error() !== JSON_ERROR_NONE) {
-            $this->error = "O arquivo JSON não é válido.";
-            Log::error("O arquivo JSON não é válido." . " Class: " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
-            return false;
-        }
-
-        foreach ($result->documentos as $key => $value) {
-            if (strlen($value->conteúdo) > $this->maxSize) {
-                $this->error = "O campo contéudo é maior do que o máximo permitido de {$this->maxSize} caracteres.";
-                Log::error("O campo contéudo é maior do que o máximo permitido de {$this->maxSize} caracteres. " . __CLASS__ . " Function: " . __FUNCTION__  .  " Line: " . __line__);
-                return false;
-            }
-        }
+        json_decode($content);
 
         return json_last_error() === JSON_ERROR_NONE;
     }
 
     public function message()
     {
-        return $this->error;
+        return 'O arquivo deve ser um JSON válido.';
     }
 }
